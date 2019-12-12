c0e39734418e1940e8b4eeca3c6fd45a137e2ffc
Ufuk Türközü <ufuk_trkz@hotmail.de>
2019-12-12 01:57:59 +0100

    Finished

--- Protocols.playground/Pages/Game.xcplaygroundpage/Contents.swift
+++ Protocols.playground/Pages/Game.xcplaygroundpage/Contents.swift
@@ -30,21 +30,142 @@ class Dice {
         return Int(generator.random() % sides) + 1
     }
 }
-
 //: Now, let's define a couple protocols for managing a dice-based game.
-
+protocol DiceGame {
+    var dice: Dice { get }
+    var players: [Player] { get set }
+    
+    func play()
+}
 
 
 //: Lastly, we'll create a custom class for tracking a player in our dice game.
-
+class Player {
+    let id: Int
+    let knockOutNumber: Int = Int.random(in: 6...9)
+    var score: Int = 0
+    var knockedOut: Bool = false
+    
+    init(id: Int) {
+        self.id = id
+    }
+    
+    
+}
 
 
 //: With all that configured, let's build our dice game class called _Knock Out!_
+class KnockOut: DiceGame {
+    var dice: Dice = Dice(sides: 6, generator: OneThroughTen())
+    
+    var players: [Player] = []
+    
+    // This is the "tracker"
+    var delegate: DiceGameDelegate?
+    
+    init(numberOfPlayers: Int) {
+        for i in 1...numberOfPlayers {
+            let aPlayer = Player(id: i)
+            players.append(aPlayer)
+        }
+    }
+    
+    func play() {
+        
+        delegate?.diceGameDidStart(game: self)
+        
+        var isGameEnded = false
+        
+        while !isGameEnded {
+            // Go trough each player
+            for player in players where player.knockedOut == false {
+            // Roll the dice
+                let diceRollSum = dice.roll() + dice.roll()
+                
+                delegate?.game(game: self, didStartNewTurnWithDiceRoll: diceRollSum)
+                
+            // Check if we are knocked out
+                if player.knockOutNumber == diceRollSum {
+                   
+                    player.knockedOut = true
+                    
+                    // Check if all of th eplayers knockedout, or if still osme players are playing
+                    
+                    var activePlayers: [Player] = []
+                    
+                    for player in activePlayers {
+                        if player.knockedOut == false {
+                            activePlayers.append(player)
+                        }
+                    }
+                    
+                    if activePlayers.count == 0 {
+                        isGameEnded = true
+                        print("All players have been knocked out!")
+                        delegate?.gameDidEnd(game: self)
+                        return
+                    }
+                // If not, add to the total score
+                } else {
+                    player.score += diceRollSum
+                    
+                    if player.score >= 100 {
+                        isGameEnded = true
+                        print ("Player \(player.id) has won with a final score of \(player.score)!")
+                        delegate?.gameDidEnd(game: self)
+                        return
+                    }
+                }
+            }
+        }
+    }
+    
+    
+}
+
+protocol DiceGameDelegate {
+   
+    func diceGameDidStart(game: DiceGame)
+    
+    func game(game: DiceGame, didStartNewTurnWithDiceRoll roll: Int)
+    
+    func gameDidEnd(game: DiceGame)
+    
+}
 
 
+class DiceGameTracker: DiceGameDelegate {
+    
+    var numberOfTurns = 0
+    
+    func diceGameDidStart(game: DiceGame) {
+        numberOfTurns = 0
+        
+        print("Started a new game!")
+    }
+    
+    func game(game: DiceGame, didStartNewTurnWithDiceRoll roll: Int) {
+        numberOfTurns += 1
+        
+        print("Rolled a \(roll).")
+    }
+    
+    func gameDidEnd(game: DiceGame) {
+        print("The game lasted \(numberOfTurns) turns.")
+    }
 
+}
+
+let myKnocOut = KnockOut(numberOfPlayers: 15)
+let tracker = DiceGameTracker()
+
+myKnocOut.delegate = tracker
+myKnocOut.play()
 //: The following class is used to track the status of the above game, and will conform to the `DiceGameDelegate` protocol.
-
+// Make the protocol (DiceGameDelegate)
+// Make the `var delegate` in the thing you want to delegate work from. (delegator) Example: KnockOut class
+// Call the delegate.someFunction at the appropriate place
+// Set the `var delegate` property to an intance of something that conforms to the delegate protocol
 
 
 //: Finally, we need to test out our game. Let's create a game instance, add a tracker, and instruct the game to play.
--- Protocols.playground/Pages/Protocol Basics.xcplaygroundpage/Contents.swift
+++ Protocols.playground/Pages/Protocol Basics.xcplaygroundpage/Contents.swift
@@ -6,15 +6,79 @@ import Foundation
 //: "... a blueprint of methods, properties, and other requirements that suit a particular task or piece of functionality. The protocol can then be adopted by a class, structure, or enumeration to provide an actual implementation of those requirements. Any type that satisfies the requirements of a protocol is said to conform to that protocol."
 //:
 //: The below example shows a protocol that requires conforming types have a particular property defined.
+// Our app needs the user's full name.
+// We can write a protocol to make sure that anything that uses the protocol has a full name.
+
+// Any ´thing insed of this protocol becomes the requirements for anyone to follow these "rules"
+
+protocol FullyName {
+    var fullName: String { get }
+    
+}
+
+struct Person: FullyName, Equatable {
+    var fullName: String
+    
+}
 
 
+struct Starship: FullyName {
+    
+    var prefix: String? // { get set }
+    var name: String // { get set }
+    
+    // Computed property - The value of the fullName will be "computed" or figured out everytime you access it.
+    var fullName: String {
+        // USS - Prefix
+        // Enterprise - Name
+        if let prefix = prefix {
+            return prefix + " " + name
+        }
+    }
+    
+    // Compare the left hand side of the "==" with the right hand side
+    static func ==(lhs: Starship, rhs: Starship) -> Bool {
+        if lhs.fullName == rhs.fullName {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    init(name: String, prefix: String? = nil) {
+        self.name = name
+        self.prefix = prefix
+    }
+}
 
+var starship = Starship(name: "Enterprise", prefix: "USS")
+
+starship.name = "Something else"
+print(starship.fullName)
 //: Protocols can also require that conforming types implement certain methods.
+protocol GeneratesRandomNumbers {
+    // we don't give the implemention of the function in the protocol
+    func random() -> Int
+}
 
+class OneThroughTen: GeneratesRandomNumbers {
+    func random() -> Int {
+          return Int.random(in: 1...10)
+      }
+}
 
+//let array = [1, 2, 3]
+//let array = array(arrayLiteral: 1, 2, 3)
 
+let numberGenerator = OneThroughTen()
+let random = numberGenerator.random()
 //: Using built-in Protocols
+let enterprise = Starship(name: "Enterprise", prefix: "USS")
+let firefly = Starship(name: "Serenity")
 
+if enterprise == firefly {
+    print("Same Starship")
+}
 
 
 //: ## Protocols as Types
